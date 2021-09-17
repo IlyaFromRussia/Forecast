65b7cef361351bb67f2606c4d2c062e11e57a914
Ilya <lobovilia1999@gmail.com>
2021-09-17 13:46:40 +0300

    Добавлен custom SeekBar

--- app/src/main/java/com/ilrcompany/forecast/activity/MainActivity.kt
+++ app/src/main/java/com/ilrcompany/forecast/activity/MainActivity.kt
@@ -2,24 +2,23 @@ package com.ilrcompany.forecast.activity
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.drawable.toDrawable
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.ilrcompany.forecast.MyLocationListener
 import com.ilrcompany.forecast.model.Model
 import com.ilrcompany.forecast.R
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.util.*
 
 
@@ -35,6 +34,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var activityLauncher : ActivityResultLauncher<Void>
     private lateinit var model : Model
     private lateinit var progresaBar : ProgressBar
+    private lateinit var leftBar : SeekBar
+    private lateinit var rightBar : SeekBar
+    private lateinit var job : Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,7 +49,8 @@ class MainActivity : AppCompatActivity() {
         cityName = findViewById(R.id.city_name)
         temperature = findViewById(R.id.temperature)
         recyclerView = findViewById(R.id.weather_day_list)
-        progresaBar = findViewById(R.id.progress_bar)
+        leftBar = findViewById(R.id.leftBar)
+        rightBar = findViewById(R.id.rightBar)
 
         navigate.setOnClickListener{
             checkPermission()
@@ -101,6 +104,7 @@ class MainActivity : AppCompatActivity() {
     private fun checkPermission(){
         val status = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
         if (status == PackageManager.PERMISSION_GRANTED){
+            startSeekBar()
             setCloserCity()
         }
         else{
@@ -112,7 +116,6 @@ class MainActivity : AppCompatActivity() {
         navigate.isClickable = false
         search.isClickable = false
         model.setCloserCity()
-        progresaBar.visibility = ProgressBar.VISIBLE
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -120,6 +123,7 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == 0){
             if (grantResults.isNotEmpty()){
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startSeekBar()
                     setCloserCity()
                 }
                 else{
@@ -131,9 +135,55 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun hideProgressBarAndActivateButton(){
-        progresaBar.visibility = ProgressBar.INVISIBLE
+        job.cancel()
+
+        leftBar.visibility = SeekBar.INVISIBLE
+        rightBar.visibility = SeekBar.INVISIBLE
         navigate.isClickable = true
         search.isClickable = true
     }
 
+    fun startSeekBar(){
+        setSeekBarProperties(leftBar)
+        setSeekBarProperties(rightBar)
+        rightBar.progress = 100
+
+        val leftBitmap = getDrawable(R.drawable.placeholder)?.toBitmap(30,30)
+        leftBar.thumb = leftBitmap?.toDrawable(resources)
+
+        val rightBitmap = getDrawable(R.drawable.earth_planet)?.toBitmap(30,30)
+        rightBar.thumb = rightBitmap?.toDrawable(resources)
+
+        leftBar.progress = 0
+        rightBar.progress = 90
+        job = GlobalScope.launch(Dispatchers.Main) {
+            var flag = true
+            while (true){
+                for (i in 0..90){
+                    delay(30)
+                    leftBar.progress = leftBar.progress + getLeftProgress(flag)
+                    rightBar.progress = rightBar.progress + getLeftProgress(!flag)
+                }
+                flag = !flag
+            }
+        }
+    }
+
+    private fun setSeekBarProperties(bar : SeekBar){
+        bar.visibility = SeekBar.VISIBLE
+        bar.isEnabled = false
+        bar.min = 0
+        bar.max = 100
+
+        val back : Drawable?
+        val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        if (currentHour in 6..19) back = getColor(R.color.DayColor).toDrawable()
+        else back = getColor(R.color.NightColor).toDrawable()
+
+        bar.background = back
+        bar.progressDrawable = back
+    }
+
+    private fun getLeftProgress(flag : Boolean) =
+        if (flag) 1 else - 1
 }
